885a63d7598637b05b783c14386181f706709509
ZachHofmeister <17202322+ZachHofmeister@users.noreply.github.com>
2026-06-14 15:36:53 -0600

    experimenting with recreating a list of posts from my website

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,12 @@
-import Image from "next/image";
-
-function Post() {
-	  return (
-		<div className="w-1/2">
-			<Image src="/annunciation.jpeg" alt="the annunciation" width={200} height={200}></Image>
-		</div>
-	  );
-}
+import Post from './posts'
 
 export default function Home() {
   return (
-	<div>
-		<h1 className="text-4xl font-bold text-center">Hello World</h1>
+	<section>
+		<h1 className="font-bold">Hello World</h1>
 		<Post />
-	</div>
+		<Post />
+		<Post />
+	</section>
   );
 }
---
+++ app/posts.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Post() {
+	  return (
+		<div className="">
+			<Image src="/annunciation.jpeg" alt="the annunciation" width={100} height={2000}></Image>
+		</div>
+	  );
+}
