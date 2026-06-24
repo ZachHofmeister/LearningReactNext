658722030a1512a3f1413a2c9471d82b3d5e246a
ZachHofmeister <17202322+ZachHofmeister@users.noreply.github.com>
2026-06-23 18:49:10 -0600

    work on navbar styling

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,14 +2,23 @@ import Link from 'next/link'
 
 export default function Navbar() {
 	return (
-		<nav className='hidden md:flex sticky top-0 inset-x-0 z-50 items-center justify-between px-8 transition-all duration-300 bg-background ring-4 ring-indigo-500 ring-inset'>
-			<Link href={'/'}>Home</Link>
-			<ul>
-				<li>Hello</li>
-				<li>Hello</li>
-				<li>Hello</li>
+		<nav className="hidden md:flex sticky top-0 inset-x-0 z-50 items-center justify-between px-8 transition-all duration-300 bg-background ring-blue-500 ring-inset">
+			<Link className="px-4 py-2 text-lg font-bold tracking-tight rounded-full" href={'/'}>Home</Link>
+			<ul className="flex items-center gap-1">
+				<li className="">
+					<Link className="px-4 py-2 rounded-full text-sm font-medium" href={'/'}>Link</Link>
+				</li>
+				<li className="">
+					<Link className="px-4 py-2 rounded-full text-sm font-medium" href={'/'}>Link</Link>
+				</li>
+				<li className="">
+					<Link className="px-4 py-2 rounded-full text-sm font-medium" href={'/'}>Link</Link>
+				</li>
+				<li className="">
+					<Link className="px-4 py-2 rounded-full text-sm font-medium" href={'/'}>Link</Link>
+				</li>
 			</ul>
-			<Link href={'/Test'}>Test</Link>
+			<Link className="px-4 py-2 text-lg font-bold tracking-tight rounded-full bg-emerald-600" href={'/Test'}>Test</Link>
 		</nav>
 	);
 }
