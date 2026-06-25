488ffa4255be442055c2d2310665fbccf82bf228
ZachHofmeister <17202322+ZachHofmeister@users.noreply.github.com>
2026-06-24 21:45:57 -0600

    some formatting adjustments to navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function Navbar() {
 	return (
-		<nav className="hidden md:flex sticky top-0 inset-x-0 z-50 items-center justify-between px-8 transition-all duration-300 bg-background ring-blue-500 ring-inset">
+		<nav className="hidden md:flex sticky top-0 inset-x-0 z-50 items-center justify-between px-8 py-2 transition-all duration-300 bg-background ring-blue-500 ring-inset">
 			<Link className="px-4 py-2 text-lg font-bold tracking-tight rounded-full" href={'/'}>Home</Link>
 			<ul className="flex items-center gap-1">
 				<li className="">
@@ -18,7 +18,7 @@ export default function Navbar() {
 					<Link className="px-4 py-2 rounded-full text-sm font-medium" href={'/'}>Link</Link>
 				</li>
 			</ul>
-			<Link className="px-4 py-2 text-lg font-bold tracking-tight rounded-full bg-emerald-600" href={'/Test'}>Test</Link>
+			<Link className="px-4 py-2 text-md font-semibold rounded-full bg-emerald-600 transition-all duration:300 hover:bg-emerald-800" href={'/Test'}>Contact Me</Link>
 		</nav>
 	);
 }
